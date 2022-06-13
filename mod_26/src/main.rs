78e855e813620fd637de5f9234b8f0860d2bd2a8
Aeshus <87614336+Aeshus@users.noreply.github.com>
2022-06-12 20:05:52 -0500

    Finished up Mod 26
    Finished moving the API to rot.rs and added a decode and encode method,
    which are basically just frontends for the underlying rotate function.
    
    I also added some more tests to work on this functionality.
    
    Finally, I added a commandline parser which just ignores the first input
    and then prints out the before and after decoding for every input after.

--- mod_26/src/main.rs
+++ mod_26/src/main.rs
@@ -1,55 +1,79 @@
-fn main() {}
+use mod_26::rot::decode;
+use std::env::args;
+
+fn main() {
+    for arg in args().skip(1) {
+        println!("{} -> {}", arg, decode(arg.clone(), 13));
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use mod_26::rot::rotate;
+    use mod_26::rot::{decode, encode};
+
+    #[test]
+    fn encode_0() {
+        let input = "abcdefghijklmnopqrstuvwxyz".to_string();
+        let output = "abcdefghijklmnopqrstuvwxyz".to_string();
+        assert_eq!(encode(input, 0), output);
+    }
+
+    #[test]
+    fn encode_3() {
+        let input = "abcdefghijklmnopqrstuvwxyz".to_string();
+        let output = "defghijklmnopqrstuvwxyzabc".to_string();
+        assert_eq!(encode(input, 3), output);
+    }
+
+    #[test]
+    fn encode_13() {
+        let input = "abcdefghijklmnopqrstuvwxyz".to_string();
+        let output = "nopqrstuvwxyzabcdefghijklm".to_string();
+        assert_eq!(encode(input, 13), output);
+    }
+
+    #[test]
+    fn encode_22() {
+        let input = "abcdefghijklmnopqrstuvwxyz".to_string();
+        let output = "wxyzabcdefghijklmnopqrstuv".to_string();
+        assert_eq!(encode(input, 22), output);
+    }
+
+    #[test]
+    fn encode_26() {
+        let input = "abcdefghijklmnopqrstuvwxyz".to_string();
+        let output = "abcdefghijklmnopqrstuvwxyz".to_string();
+        assert_eq!(encode(input, 26), output);
+    }
+
+    #[test]
+    fn encode_34() {
+        let input = "abcdefghijklmnopqrstuvwxyz".to_string();
+        let output = "ijklmnopqrstuvwxyzabcdefgh".to_string();
+        assert_eq!(encode(input, 34), output);
+    }
+
+    #[test]
+    fn encode_200() {
+        let input = "abcdefghijklmnopqrstuvwxyz".to_string();
+        let output = "stuvwxyzabcdefghijklmnopqr".to_string();
+        assert_eq!(encode(input, 200), output);
+    }
 
     #[test]
     fn rot_0() {
         let input = "abcdefghijklmnopqrstuvwxyz".to_string();
-        let output = "abcdefghijklmnopqrstuvwxyz".to_string();
-        assert_eq!(rotate(input, 0), output);
+        assert_eq!(decode(encode(input.clone(), 0), 0), input);
+    }
+    #[test]
+    fn rot_255() {
+        let input = "abcdefghijklmnopqrstuvwxyz".to_string();
+        assert_eq!(decode(encode(input.clone(), 255), 255), input);
     }
 
     #[test]
-    fn rot_3() {
+    fn rot_512() {
         let input = "abcdefghijklmnopqrstuvwxyz".to_string();
-        let output = "defghijklmnopqrstuvwxyzabc".to_string();
-        assert_eq!(rotate(input, 3), output);
-    }
-
-    #[test]
-    fn rot_13() {
-        let input = "abcdefghijklmnopqrstuvwxyz".to_string();
-        let output = "nopqrstuvwxyzabcdefghijklm".to_string();
-        assert_eq!(rotate(input, 13), output);
-    }
-
-    #[test]
-    fn rot_22() {
-        let input = "abcdefghijklmnopqrstuvwxyz".to_string();
-        let output = "wxyzabcdefghijklmnopqrstuv".to_string();
-        assert_eq!(rotate(input, 22), output);
-    }
-
-    #[test]
-    fn rot_26() {
-        let input = "abcdefghijklmnopqrstuvwxyz".to_string();
-        let output = "abcdefghijklmnopqrstuvwxyz".to_string();
-        assert_eq!(rotate(input, 26), output);
-    }
-
-    #[test]
-    fn rot_34() {
-        let input = "abcdefghijklmnopqrstuvwxyz".to_string();
-        let output = "ijklmnopqrstuvwxyzabcdefgh".to_string();
-        assert_eq!(rotate(input, 34), output);
-    }
-
-    #[test]
-    fn rot_200() {
-        let input = "abcdefghijklmnopqrstuvwxyz".to_string();
-        let output = "stuvwxyzabcdefghijklmnopqr".to_string();
-        assert_eq!(rotate(input, 200), output);
+        assert_eq!(decode(encode(input.clone(), 512), 512), input);
     }
 }
--- mod_26/src/rot.rs
+++ mod_26/src/rot.rs
@@ -1,5 +1,7 @@
-pub fn rotate(input: String, shift: u8) -> String {
+pub fn rotate(input: String, shift: usize) -> String {
     let mut output = String::new();
+
+    let sanitized_shift: u8 = (shift % 26).try_into().unwrap();
 
     for character in input.chars() {
         let case = match character {
@@ -11,10 +13,18 @@ pub fn rotate(input: String, shift: u8) -> String {
             }
         };
 
-        let output_char = (case + (character as u8 - case + shift) % 26) as char;
+        let output_char = (case + (character as u8 - case + sanitized_shift) % 26) as char;
 
         output.push(output_char);
     }
 
     output
+}
+
+pub fn decode(input: String, shift: usize) -> String {
+    rotate(input, 26 - (shift % 26))
+}
+
+pub fn encode(input: String, shift: usize) -> String {
+    rotate(input, shift)
 }
